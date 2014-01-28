6b7b1e0510964c87bedd5fae726e1ee55726f129
Hanson <hanson.lin1@gmail.com>
2014-01-28 04:40:14 -0500

    Hopefully added in functionality for finding places to hide bodies, unable to test for now

--- app.py
+++ app.py
@@ -83,7 +83,23 @@ def maps():
       };
       var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
-      }
+      
+var request = {
+location: center
+radius: '500'
+query: 'park'
+};
+  service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      var place = results[i];
+      createMarker(results[i]);
+    }
+  }
+}
       google.maps.event.addDomListener(window, 'load', initialize);
 </script>
       """    
